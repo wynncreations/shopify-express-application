709434096bae714529efd2a3d3b49237de3daabe
wynncreations <wynncreations@gmail.com>
2019-04-15 18:36:55 -0500

    initial commit of tutorial express shopify app

---
+++ app.js
@@ -0,0 +1,39 @@
+const dotenv = require('dotenv').config();
+const express = require('express');
+const app = express();
+const crypto = require('crypto');
+const cookie = require('cookie');
+const nonce = require('nonce')();
+const querystring = require('querystring');
+const request = require('request-promise');
+
+const apiKey = process.env.SHOPIFY_API_KEY;
+const apiSecret = process.env.SHOPIFY_API_SECRET;
+const scopes = 'read_products';
+const forwardingAddress = "https://betterorders.heroku.com/shopify"; // Replace this with your HTTPS Forwarding address
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.get('/shopify', (req, res) => {
+    const shop = req.query.shop;
+    if (shop) {
+        const state = nonce();
+        const redirectUri = forwardingAddress + '/shopify/callback';
+        const installUrl = 'https://' + shop +
+            '/admin/oauth/authorize?client_id=' + apiKey +
+            '&scope=' + scopes +
+            '&state=' + state +
+            '&redirect_uri=' + redirectUri;
+
+        res.cookie('state', state);
+        res.redirect(installUrl);
+    } else {
+        return res.status(400).send('Missing shop parameter. Please add ?shop=your-development-shop.myshopify.com to your request');
+    }
+});
+
+app.listen(3000, () => {
+    console.log('Example app listening on port 3000!');
+});
