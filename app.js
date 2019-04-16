04f0ee63b9ce960c1231d6e8b87460166fc46650
wynncreations <wynncreations@gmail.com>
2019-04-15 20:41:04 -0500

    starting the customization of the default app.

--- app.js
+++ app.js
@@ -6,107 +6,15 @@ const cookie = require('cookie');
 const nonce = require('nonce')();
 const querystring = require('querystring');
 const request = require('request-promise');
+const index = require('./Routes/index');
 
 const apiKey = process.env.apiKey;
 const apiSecret = process.env.apiSecret;
 const scopes = 'read_products';
 const forwardingAddress = "https://betterorders.herokuapp.com/shopify"; // Replace this with your HTTPS Forwarding address
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-});
+app.use('/',index);
 
-app.get('/shopify', (req, res) => {
-    const shop = req.query.shop;
-    if (shop) {
-        const state = nonce();
-        const redirectUri = forwardingAddress + '/callback';
-        const installUrl = 'https://' + shop +
-            '/admin/oauth/authorize?client_id=' + apiKey +
-            '&scope=' + scopes +
-            '&state=' + state +
-            '&redirect_uri=' + redirectUri;
-
-        res.cookie('state', state);
-        res.redirect(installUrl);
-    } else {
-        return res.status(400).send('Missing shop parameter. Please add ?shop=your-development-shop.myshopify.com to your request');
-    }
-});
-app.get('/shopify/callback', (req, res) => {
-    const {
-        shop,
-        hmac,
-        code,
-        state
-    } = req.query;
-    const stateCookie = cookie.parse(req.headers.cookie).state;
-
-    if (state !== stateCookie) {
-        return res.status(403).send('Request origin cannot be verified');
-    }
-
-    if (shop && hmac && code) {
-        const map = Object.assign({}, req.query);
-        delete map['signature'];
-        delete map['hmac'];
-        const message = querystring.stringify(map);
-        const providedHmac = Buffer.from(hmac, 'utf-8');
-        const generatedHash = Buffer.from(
-            crypto
-            .createHmac('sha256', apiSecret)
-            .update(message)
-            .digest('hex'),
-            'utf-8'
-        );
-        let hashEquals = false;
-        // timingSafeEqual will prevent any timing attacks. Arguments must be buffers
-        try {
-            hashEquals = crypto.timingSafeEqual(generatedHash, providedHmac)
-            // timingSafeEqual will return an error if the input buffers are not the same length.
-        } catch (e) {
-            hashEquals = false;
-        };
-
-        if (!hashEquals) {
-            return res.status(400).send('HMAC validation failed');
-        }
-
-        const accessTokenRequestUrl = 'https://' + shop + '/admin/oauth/access_token';
-        const accessTokenPayload = {
-            client_id: apiKey,
-            client_secret: apiSecret,
-            code,
-        };
-
-        request.post(accessTokenRequestUrl, {
-                json: accessTokenPayload
-            })
-            .then((accessTokenResponse) => {
-                const accessToken = accessTokenResponse.access_token;
-
-const shopRequestUrl = 'https://' + shop + '/admin/api/2019-04/shop.json';
-const shopRequestHeaders = {
-    'X-Shopify-Access-Token': accessToken,
-};
-
-request.get(shopRequestUrl, {
-        headers: shopRequestHeaders
-    })
-    .then((shopResponse) => {
-        res.end(shopResponse);
-    })
-    .catch((error) => {
-        res.status(error.statusCode).send(error.error.error_description);
-    });
-            })
-            .catch((error) => {
-                res.status(error.statusCode).send(error.error.error_description);
-            });
-    } else {
-        res.status(400).send('Required parameters missing');
-    }
-});
 app.listen(process.env.PORT || 8080, () => {
     console.log('Example app listening on port ' + process.env.PORT || 8080  + ' !');
 });
